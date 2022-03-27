7472c967562b96cc579f7319c49dc431803e0e75
StylexTV <56502218+StylexTV@users.noreply.github.com>
2022-03-27 09:57:59 +0200

    Create MoveProcessor.java

---
+++ src/de/stylextv/haskal/game/move/MoveProcessor.java
@@ -0,0 +1,25 @@
+package de.stylextv.haskal.game.move;
+
+import de.stylextv.haskal.game.board.Board;
+
+public class MoveProcessor {
+	
+	private Board board;
+	
+	public MoveProcessor(Board board) {
+		this.board = board;
+	}
+	
+	public void makeMove(int move) {
+		
+	}
+	
+	public void unmakeMove(int move) {
+		
+	}
+	
+	public Board getBoard() {
+		return board;
+	}
+	
+}
